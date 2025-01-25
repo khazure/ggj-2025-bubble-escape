b6932a108379aa4c572bf012d94450fa2d5f560d
khazure <35125073+khazure@users.noreply.github.com>
2025-01-25 11:18:36 -0800

    added slow movement to player ball

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -1,16 +1,50 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class Player_Controller : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
+    //2. Assign the new Rigidbody variable
+    private Rigidbody rb;
+
+    //Movement Variables
+    private float movementX;
+    private float movementY;
+
+
+    private void FixedUpdate() 
+    {
+        Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
+
+        // Add force to the player
+        rb.AddForce(movement);
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent <Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    //Function plays on movement?
+    void OnMove (InputValue movementValue) 
     {
-        
+        //Applying input data to the Player
+        //1. Set the movement vector
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        //Assign the movement variables
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+
     }
+
+    //3. Add a FixedUpdate function
+
+    // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+
+
 }
