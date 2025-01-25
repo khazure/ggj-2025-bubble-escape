32eda29719ee8564633c015b7213e476090024d8
khazure <35125073+khazure@users.noreply.github.com>
2025-01-25 14:39:49 -0800

    non buggy jump completed

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
 
     public float jumpAmount = 0; 
 
+    public float gravityScale = 0;
+
     //2. Assign the new Rigidbody variable
     private Rigidbody rb;
 
@@ -15,19 +17,23 @@ public class PlayerController : MonoBehaviour
     private float movementX;
     private float movementY;
 
+    private bool isGrounded;
+
 
     private void FixedUpdate() 
     {
         Vector3 movement = new Vector3 (movementX, 0.0f, movementY) * acc;
-        
+        Vector3 gravity = Physics.gravity * (gravityScale - 1) * rb.mass;
+
         // Add force to the player
-        Vector3 force = movement;
+        Vector3 force = movement + gravity;
         rb.AddForce(force);
     }
 
     private void Update() 
-    {
-        if (Input.GetKeyDown(KeyCode.Space)) 
+    {   
+        bool grounded = (Physics.Raycast(rb.position, Vector3.down, 1f)); // raycast down to look for ground is not detecting ground? only works if allowing jump when grounded = false; // return "Ground" layer as layer
+        if (Input.GetKeyDown(KeyCode.Space) && grounded) 
         {
             rb.AddForce(Vector3.up * jumpAmount, ForceMode.Impulse);
         }
@@ -61,5 +67,18 @@ public class PlayerController : MonoBehaviour
         
     // }
 
+    // void OnTriggerStay(Collider other)
+    // {
+    //     if (other.transform.tag == "ground")
+    //     {
+    //         isGrounded = true;
+    //         Debug.Log("Grounded");
+    //     }
+    //     else
+    //     {
+    //         isGrounded = false;
+    //         Debug.Log("Not Grounded!");
+    //     }
+    // }
 
 }
