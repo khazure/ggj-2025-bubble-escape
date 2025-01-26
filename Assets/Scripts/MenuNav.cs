f2c99fdb36a1073ae98944ba6abc1bb361ec4dad
khazure <35125073+khazure@users.noreply.github.com>
2025-01-26 12:49:09 -0800

    made start screen

---
+++ Assets/Scripts/MenuNav.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenuNav : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Changes the scene based on the sceneID available in the build list
+    public void moveToScene(int sceneID) {
+        SceneManager.LoadSceneAsync(sceneID);
+    }
+
+}
