72cf9bbaedd306ef52d92fa60d06e90334b13fe0
birehema <birehema.sadala@bellevuecollege.edu>
2025-01-25 17:16:31 -0800

    Win Scene
    winning scene

--- Assets/Scripts/EscapeTrigger.cs
+++ Assets/Scripts/EscapeTrigger.cs
@@ -5,7 +5,7 @@ public class EscapeTrigger : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("player"))
         {
             // Disable player movement
             other.GetComponent<PlayerController>().enabled = false;
